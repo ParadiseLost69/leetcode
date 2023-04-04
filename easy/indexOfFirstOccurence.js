2dcce6f3642d8770f993fcffe510e2c02e5b67ed
Teddy <tedloukas12@gmail.com>
2023-04-04 13:41:05 -0400

    Index of first occurence

---
+++ easy/indexOfFirstOccurence.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  //find the occurence of the word and output the index at which it occurs
+
+  //if it does not occur output -1
+
+  return haystack.indexOf(needle);
+};
